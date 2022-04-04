c1b8f539b92d098a1b70febf34820e5790f18dd2
musaa99 <musaopaluwa@gmail.com>
2022-04-04 22:58:32 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Head from 'next/head';
+function MyApp({Component, pageProps}) {
+	return (
+		<>
+			<Head>
+				<title>RapidAPI Devrel Example - Real Estate App</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
-
-export default MyApp
+export default MyApp;
